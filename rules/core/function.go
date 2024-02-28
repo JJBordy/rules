4619cf8e0289828d7f112fe784746000506be843
ionbo <ionbordian@hotmail.com>
2024-02-28 22:36:12 +0200

    Test for functions

--- rules/core/function.go
+++ rules/core/function.go
@@ -16,16 +16,3 @@ type RuleFunction struct {
 func (c RuleFunction) Execute(input any) (bool, error) {
 	return c.Function(input, c.Args)
 }
-
-// RuleFunctionOfList - a function contained in a condition of rule
-// functions.FunctionOfList implementations are defined in (go) code, while RuleFunction is read from the rule (yaml)
-type RuleFunctionOfList struct {
-	Key            string
-	Args           []any
-	FunctionOfList functions.FunctionOfList
-}
-
-// ExecuteOfList - executes the contained functions.ExecuteOfList with the arguments specified in the rule
-func (c RuleFunctionOfList) ExecuteOfList(inputs []any) (bool, error) {
-	return c.FunctionOfList(inputs, c.Args)
-}
--- rules/functions/single_input_test.go
+++ rules/functions/single_input_test.go
@@ -1,6 +1,7 @@
 package functions
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -16,16 +17,274 @@ func TestDefaultSingleInputFunctions(t *testing.T) {
 		ExpectOutput bool
 		ExpectError  bool
 	}{
-		{},
+		{
+			FuncName:     Empty,
+			Args:         []any{},
+			Input:        "",
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     Empty,
+			Args:         []any{},
+			Input:        "SOMETHING",
+			ExpectOutput: false,
+		},
+
+		{
+			FuncName:     NonEmpty,
+			Args:         []any{},
+			Input:        "",
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     NonEmpty,
+			Args:         []any{},
+			Input:        "SOMETHING",
+			ExpectOutput: true,
+		},
+
+		{
+			FuncName:     Equal,
+			Args:         []any{"SOMETHING"},
+			Input:        "SOMETHING",
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     Equal,
+			Args:         []any{"SOMETHING"},
+			Input:        "SOMETHINGELSE",
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     Greater,
+			Args:         []any{1},
+			Input:        2,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     Greater,
+			Args:         []any{1},
+			Input:        0,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     Greater,
+			Args:         []any{1},
+			Input:        1,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     GreaterEq,
+			Args:         []any{1},
+			Input:        2,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     GreaterEq,
+			Args:         []any{1},
+			Input:        1,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     GreaterEq,
+			Args:         []any{1},
+			Input:        0,
+			ExpectOutput: false,
+		},
+
+		{
+			FuncName:     Lower,
+			Args:         []any{1},
+			Input:        0,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     Lower,
+			Args:         []any{1},
+			Input:        1,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     Lower,
+			Args:         []any{1},
+			Input:        2,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     LowerEq,
+			Args:         []any{1},
+			Input:        0,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     LowerEq,
+			Args:         []any{1},
+			Input:        1,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     LowerEq,
+			Args:         []any{1},
+			Input:        2,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     Between,
+			Args:         []any{1, 2},
+			Input:        1,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     Between,
+			Args:         []any{1, 3},
+			Input:        2,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     Between,
+			Args:         []any{1, 2},
+			Input:        3,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     BetweenEq,
+			Args:         []any{1, 2},
+			Input:        1,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     BetweenEq,
+			Args:         []any{1, 3},
+			Input:        2,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     BetweenEq,
+			Args:         []any{1, 3},
+			Input:        3,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     BetweenEq,
+			Args:         []any{1, 3},
+			Input:        4,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     BetweenEq,
+			Args:         []any{1, 3},
+			Input:        0,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     NotBetween,
+			Args:         []any{1, 2},
+			Input:        3,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     NotBetween,
+			Args:         []any{1, 2},
+			Input:        2,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     NotBetween,
+			Args:         []any{1, 2},
+			Input:        1,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     NotBetweenEq,
+			Args:         []any{1, 2},
+			Input:        3,
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     NotBetweenEq,
+			Args:         []any{1, 2},
+			Input:        2,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     NotBetweenEq,
+			Args:         []any{1, 2},
+			Input:        1,
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     EqualIgnoreCase,
+			Args:         []any{"a", "a"},
+			Input:        "A",
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     EqualIgnoreCase,
+			Args:         []any{"a", "a"},
+			Input:        "b",
+			ExpectOutput: false,
+		},
+		{
+			FuncName:     EqualAny,
+			Args:         []any{"a", "xyz", "ABC"},
+			Input:        "ABC",
+			ExpectOutput: true,
+		},
+		{
+			FuncName:     EqualAny,
+			Args:         []any{"a", "xyz", "ABC"},
+			Input:        "DEF",
+			ExpectOutput: false,
+		},
 	}
 
+	/*
+		// Greater - the input is a number and is greater than the argument
+		Greater = "Greater"
+		// GreaterEq - the input is a number and is greater or equal to the argument
+		GreaterEq = "GreaterEq"
+		// Lower - the input is a number and is lower than the argument
+		Lower = "Lower"
+		// LowerEq - the input is a number and is lower or equal to the argument
+		LowerEq = "LowerEq"
+		// Between - the input is a number and is between the two arguments
+		Between = "Between"
+		// BetweenEq - the input is a number and is between or equal to the two arguments
+		BetweenEq = "BetweenEq"
+		// NotBetween - the input is a number and is not between the two arguments
+		NotBetween = "NotBetween"
+		// NotBetweenEq - the input is a number and is not between or equal to the two arguments
+		NotBetweenEq = "NotBetweenEq"
+
+		// EqualIgnoreCase - the input is equal to the argument; case-insensitive
+		EqualIgnoreCase = "EqualIgnoreCase"
+		// EqualAny - the input is equal to any of the arguments
+		EqualAny = "EqualAny"
+		// NotEqualAny - the input is not equal to any of the arguments
+		NotEqualAny = "NotEqualAny"
+		// StartsWith - the input starts with the argument
+		StartsWith = "StartsWith"
+		// StartsWithIgnoreCase - the input starts with the argument; case-insensitive
+		StartsWithIgnoreCase = "StartsWithIgnoreCase"
+		// EndsWith - the input ends with the argument
+		EndsWith = "EndsWith"
+		// EndsWithIgnoreCase - the input ends with the argument; case-insensitive
+		EndsWithIgnoreCase = "EndsWithIgnoreCase"
+		// Contains - the input contains the argument
+		Contains = "Contains"
+		// ContainsIgnoreCase - the input contains the argument; case-insensitive
+		ContainsIgnoreCase = "ContainsIgnoreCase"
+	*/
+
 	for _, tc := range testCases {
-		result, err := defaultFunctions[tc.FuncName](tc.Input, tc.Args)
-		if tc.ExpectError {
-			assert.Equal(t, tc.ExpectOutput, result)
-			assert.Nil(t, err)
-		} else {
-			assert.NotNil(t, err)
-		}
+		t.Run(tc.FuncName, func(t *testing.T) {
+			result, err := defaultFunctions[tc.FuncName](tc.Input, tc.Args)
+			assert.Equal(t, tc.ExpectOutput, result, fmt.Sprintf("%+v\n", tc))
+			if tc.ExpectError {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err, fmt.Sprintf("%+v\n", tc), fmt.Sprint(err))
+			}
+		})
 	}
 }
